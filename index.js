7883860cedaf6ffff07be711eb3ca92918a3a912
Sinanmalik <sinanmalik3113@gmail.com>
2024-02-08 10:34:12 -0500

    complete lab

---
+++ index.js
@@ -0,0 +1,19 @@
+// Define the divide function
+const divide = (x, y) => {
+    return x / y;
+  };
+  
+  // Define the square arrow function
+  const square = (num) => {
+    return num * num;
+  };
+  
+  // Define the add arrow function
+  const add = (a, b) => {
+    return a + b;
+  };
+  
+  // Example usage:
+  console.log(divide(2000, 100)); // Output: 20
+  console.log(square(5)); // Output: 25
+  console.log(add(10, 5)); // Output: 15
